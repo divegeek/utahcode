a268529c7cb0e2ab6f1c9ef282c8aa259013dbc1
Shawn Willden <shawn@willden.org>
2010-03-18 10:51:33 -0600

    Modified update script to skip commit if nothing changed.

--- src/update_utahcode.sh
+++ src/update_utahcode.sh
@@ -5,7 +5,10 @@ git pull origin master
 rm -rf code constitution
 python /home/shawn/sources/utahcode/src/retrieve_code.py .
 git add code constitution
-git commit -m "`date`" -a
-git tag -f -a -m "Daily tag" `date +"%F"`
-git push --tag origin master
+git status > /dev/null
+if [ $? -eq 0 ]; then
+    git commit -m "`date`" -a
+    git tag -f -a -m "Daily tag" `date +"%F"`
+    git push --tag origin master
+fi
 popd
