bb80dfadfe3c09baa158a7cc5f8517c54bd900a7
Shawn Willden <shawn@willden.org>
2012-05-28 18:05:51 -0600

    Adjusting to (hopefully) e-mail me only when the cron job fails or pushes new changes.

--- src/update_utahcode.sh
+++ src/update_utahcode.sh
@@ -1,14 +1,15 @@
-#!/bin/sh
-date
-pushd ~/sources/utahcode
-git pull origin master
+#!/bin/sh -e
+LOGFILE=~/log/utahcode.log
+touch $LOGFILE
+date >> $LOGFILE
+cd ~/sources/utahcode
+git pull -q origin master
 rm -rf code constitution
-python /home/shawn/sources/utahcode/src/retrieve_code.py .
-git add code constitution
-git status > /dev/null
+python /home/shawn/sources/utahcode/src/retrieve_code.py . >> $LOGFILE
+git add code constitution >> $LOGFILE
+git status >> $LOGFILE
 if [ $? -eq 0 ]; then
     git commit -m "`date`" -a
     git tag -f -a -m "Daily tag" `date +"%F"`
     git push --tag origin master
 fi
-popd
