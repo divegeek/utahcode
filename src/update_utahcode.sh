087eb6124691746ed1fff830554d377ba0258313
Shawn Willden <shawn@willden.org>
2012-05-28 18:34:13 -0600

    Modified testing of status
    Apparently, the exit code from "git status" isn't useful.  However,
    with the --porcelain option, it produces no output if there are no
    changes, so I changed the script to test for output.

--- src/update_utahcode.sh
+++ src/update_utahcode.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/sh
 LOGFILE=~/log/utahcode.log
 touch $LOGFILE
 date >> $LOGFILE
@@ -7,9 +7,10 @@ git pull -q origin master
 rm -rf code constitution
 python /home/shawn/sources/utahcode/src/retrieve_code.py . >> $LOGFILE
 git add code constitution >> $LOGFILE
-git status >> $LOGFILE
-if [ $? -eq 0 ]; then
-    git commit -m "`date`" -a
+DIFFLOG=`git status --porcelain`
+if [ ! -z "$DIFFLOG" ]; then
+    echo "$DIFFLOG" >> $LOGFILE
+    git commit -m "`date`"
     git tag -f -a -m "Daily tag" `date +"%F"`
     git push --tag origin master
 fi
