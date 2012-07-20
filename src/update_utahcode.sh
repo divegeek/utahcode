24a8b0030ac8c8dc90627b8d9e4b1ff0f2b9a9e0
Shawn Willden <shawn@willden.org>
2012-07-20 09:14:41 -0600

    Add -A flag to "git add" to remove deleted files.

--- src/update_utahcode.sh
+++ src/update_utahcode.sh
@@ -6,7 +6,7 @@ cd ~/sources/utahcode
 git pull -q origin master
 rm -rf code constitution
 python /home/shawn/sources/utahcode/src/retrieve_code.py . >> $LOGFILE
-git add code constitution >> $LOGFILE
+git add -A code constitution >> $LOGFILE
 DIFFLOG=`git status --porcelain`
 if [ ! -z "$DIFFLOG" ]; then
     echo "$DIFFLOG" >> $LOGFILE
